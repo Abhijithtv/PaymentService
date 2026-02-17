36393c2636c86499581e45324911a8abc290c9ad
Abhijith T V <abhijithtvrajan2001@gmail.com>
2026-02-17 12:08:32 +0530

    feat: Add v5

--- PaymentAPI/Controllers/WeatherForecastController.cs
+++ PaymentAPI/Controllers/WeatherForecastController.cs
@@ -65,5 +65,17 @@ namespace PaymentAPI.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet("/GetWeatherForecastV5")]
+        public IEnumerable<WeatherForecast> GetV5()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
