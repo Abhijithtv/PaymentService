f40205a25ce9b7016af8d2d41b2d6bd4db38247e
Abhijith T V <abhijithtvrajan2001@gmail.com>
2026-02-23 12:17:21 +0530

    feat: Add data archival web job and enable swagger for release build

---
+++ DataArchivalJob/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataArchivalJob
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started Data Archival Job");
+            Console.WriteLine("Establishing DB Connection");
+            Console.WriteLine("Archival Progress_____10%");
+            Console.WriteLine("Archival Progress_____30%");
+            Console.WriteLine("Archival Progress_____60%");
+            Console.WriteLine("Archival Progress_____80%");
+            Console.WriteLine("Archival Progress_____100%");
+            Console.WriteLine("Archival Completed");
+        }
+    }
+}
