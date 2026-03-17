df78ec2ba1d2c92d0f19f3579e3af305400f9e9f
Abhijith T V <abhijithtvrajan2001@gmail.com>
2026-03-17 23:06:57 +0530

    feat: Long Durable function with timer

---
+++ UserAzureFunctions/Functions/Durable/LongRunningWithTimer/LongRunningWithTimer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+
+namespace UserAzureFunctions.Functions.Durable.LongRunningWithTimer
+{
+    public class LongRunningWithTimer
+    {
+        [Function(nameof(LongRunningWithTimer))]
+        public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "api/v2/user-on-board")] HttpRequestData requestData,
+            [DurableClient] DurableTaskClient client)
+        {
+            var instanceId = await client.ScheduleNewOrchestrationInstanceAsync(nameof(UserOnboarderV2), requestData.Query["userName"]);
+            return await client.CreateCheckStatusResponseAsync(requestData, instanceId);
+        }
+
+        [Function(nameof(UserOnboarderV2))]
+        public static async Task<Tuple<string?, string>> UserOnboarderV2([OrchestrationTrigger] TaskOrchestrationContext context)
+        {
+            var userName = context.GetInput<string>();
+            var dateTime = context.CurrentUtcDateTime.AddSeconds(60);
+            var timerTask = context.CreateTimer(dateTime, CancellationToken.None);
+            var kycStatusTask = context.WaitForExternalEvent<bool>("KycCompleted");
+
+            var winner = await Task.WhenAny(kycStatusTask, timerTask);
+
+            bool kycStatus = false;
+
+            if (winner == kycStatusTask)
+            {
+                kycStatus = await kycStatusTask;
+            }
+
+            return Tuple.Create(userName,
+                kycStatus ?
+                    "Account Creation Completed" :
+                    "KYC is pending");
+        }
+
+    }
+}
